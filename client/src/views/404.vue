0a5c6d66ae61cde7f4b9b7d6bdfd951d4766ff06
Akib Reza Khan <33821998+akibrezakhan@users.noreply.github.com>
2020-09-04 15:21:55 +0600

    Added 404 error view

---
+++ client/src/views/404.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="view"></div>
+</template>
+
+<style lang="scss" scoped>
+.view {
+  background-image: url("../assets/img/404.svg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+</style>
